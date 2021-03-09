6960df4ad2b5df73c184cd30dd64179b15b787b6
ash0419 <ash0419@naver.com>
2021-03-09 21:44:39 +0900

    2021.03.09

--- src/main/resources/static/js/cs/cs_write.js
+++ src/main/resources/static/js/cs/cs_write.js
@@ -1,132 +1,132 @@
 // 클릭 이벤트
 function on() {
-  document.querySelector("#overlay").style.display = "block";
+  document.querySelector("#overlay").style.display = "block"
 }
 
 function off() {
-  document.querySelector("#overlay").style.display = "none";
+  document.querySelector("#overlay").style.display = "none"
 }
 
 //해시태그
 function hashtag(event) {
-  var input_test = document.querySelector(".text_hashtag");
-  var input_ht = input_test.value;
-  var span_ht = document.createElement("span");
-  span_ht.className = "span_ht";
-  var div_DeleteBtn2 = document.createElement("div");
-  div_DeleteBtn2.className = "DeleteBtn2";
-  div_DeleteBtn2.innerText = "x";
+  var input_test = document.querySelector(".text_hashtag")
+  var input_ht = input_test.value
+  var span_ht = document.createElement("span")
+  span_ht.className = "span_ht"
+  var div_DeleteBtn2 = document.createElement("div")
+  div_DeleteBtn2.className = "DeleteBtn2"
+  div_DeleteBtn2.innerText = "x"
   div_DeleteBtn2.addEventListener("click", function () {
-    span_ht.remove();
-  });
-  var input = document.createElement("input");
-  input.className = "text_hashtag";
-  input.type = "text";
-  span_ht.innerText = sarp(input_ht);
-  span_ht.append(div_DeleteBtn2);
-  document.querySelector("#write_hashtag").appendChild(span_ht);
-  input_test.before(span_ht);
-  input_test.value = "";
+    span_ht.remove()
+  })
+  var input = document.createElement("input")
+  input.className = "text_hashtag"
+  input.type = "text"
+  span_ht.innerText = sarp(input_ht)
+  span_ht.append(div_DeleteBtn2)
+  document.querySelector("#write_hashtag").appendChild(span_ht)
+  input_test.before(span_ht)
+  input_test.value = ""
 }
 
 //맨앞에 #붙이기
 function sarp(input_ht) {
   if (input_ht.charAt(0) !== "#") {
-    input_ht = "#" + input_ht;
-    return input_ht;
+    input_ht = "#" + input_ht
+    return input_ht
   }
-  return input_ht;
+  return input_ht
 }
 
 //해시태그에서 엔터, 스페이스바치면 넘어가게 처리
 function enterkey() {
   if (window.event.keyCode == 32 || window.event.keyCode == 13) {
-    hashtag();
+    hashtag()
   }
 }
 
 //preview_img
 function setThumbnail(event) {
   for (var image of event.target.files) {
-    var reader = new FileReader();
+    var reader = new FileReader()
 
     reader.onload = function (event) {
-      var div_preview = document.createElement("div");
-      div_preview.className = "preview";
-      var div_DeleteBtn = document.createElement("div");
-      div_DeleteBtn.className = "DeleteBtn";
-      div_DeleteBtn.innerText = "x";
+      var div_preview = document.createElement("div")
+      div_preview.className = "preview"
+      var div_DeleteBtn = document.createElement("div")
+      div_DeleteBtn.className = "DeleteBtn"
+      div_DeleteBtn.innerText = "x"
       div_DeleteBtn.addEventListener("click", function () {
-        div_preview.remove();
-      });
-      var img = document.createElement("img");
-      img.setAttribute("src", event.target.result);
-      div_preview.append(div_DeleteBtn);
-      div_preview.append(img);
-      document.querySelector("div.write_preview").appendChild(div_preview);
-    };
+        div_preview.remove()
+      })
+      var img = document.createElement("img")
+      img.setAttribute("src", event.target.result)
+      div_preview.append(div_DeleteBtn)
+      div_preview.append(img)
+      document.querySelector("div.write_preview").appendChild(div_preview)
+    }
 
-    console.log(image);
-    reader.readAsDataURL(image);
+    console.log(image)
+    reader.readAsDataURL(image)
   }
 }
 
 //preview_video
 function setThumbnailvideo(event) {
   for (var video of event.target.files) {
-    var reader = new FileReader();
+    var reader = new FileReader()
 
     reader.onload = function (event) {
-      var div_preview = document.createElement("div");
-      div_preview.className = "preview";
-      var div_DeleteBtn = document.createElement("div");
-      div_DeleteBtn.className = "DeleteBtn";
-      div_DeleteBtn.innerText = "x";
+      var div_preview = document.createElement("div")
+      div_preview.className = "preview"
+      var div_DeleteBtn = document.createElement("div")
+      div_DeleteBtn.className = "DeleteBtn"
+      div_DeleteBtn.innerText = "x"
       div_DeleteBtn.addEventListener("click", function () {
-        div_preview.remove();
-      });
-      var video = document.createElement("video");
-      video.setAttribute("src", event.target.result);
+        div_preview.remove()
+      })
+      var video = document.createElement("video")
+      video.setAttribute("src", event.target.result)
       if (video.paused) {
-        video.play();
+        video.play()
       } else {
-        video.pause();
+        video.pause()
       }
-      div_preview.append(div_DeleteBtn);
-      div_preview.append(video);
-      document.querySelector("div.write_preview").appendChild(div_preview);
-    };
+      div_preview.append(div_DeleteBtn)
+      div_preview.append(video)
+      document.querySelector("div.write_preview").appendChild(div_preview)
+    }
 
-    console.log(video);
-    reader.readAsDataURL(video);
+    console.log(video)
+    reader.readAsDataURL(video)
   }
 }
 // ------------------------------------------------------cs-----------------------------------------
 //돌아가기
 
 function return_page() {
-  history.back();
+  history.back()
 }
 
 //공지사항 등록
-const notice_write_form = document.querySelector("#notice_write_form");
+const notice_write_form = document.querySelector("#notice_write_form")
 
 function notice_write_submit_btn() {
   if (!notice_write_form.notice_pk.value) {
-    noticeReg();
-    return;
+    noticeReg()
+    return
   }
-  noticeUpd();
+  noticeUpd()
 }
 
 function noticeReg() {
-  ajax();
+  ajax()
   function ajax() {
     let param = {
       notice_title: notice_write_form.notice_title.value,
       notice_ctnt: notice_write_form.notice_ctnt.value,
       notice_userpk: notice_write_form.notice_userpk.value,
-    };
+    }
     fetch("/notice_write", {
       method: "post",
       headers: {
@@ -136,24 +136,24 @@ function noticeReg() {
     })
       .then((res) => res.json())
       .then(function (myJson) {
-        console.log(myJson);
+        console.log(myJson)
         if (myJson.result === 1) {
-          location.href = `/notice`;
+          location.href = `/notice`
         }
-      });
+      })
   }
 }
 
 //공지사항 수정
 function noticeUpd() {
-  ajax();
+  ajax()
   function ajax() {
     let param = {
       notice_title: notice_write_form.notice_title.value,
       notice_ctnt: notice_write_form.notice_ctnt.value,
       notice_pk: notice_write_form.notice_pk.value,
-    };
-    console.log(param);
+    }
+    console.log(param)
     fetch("/notice_upd", {
       method: "post",
       headers: {
@@ -163,32 +163,32 @@ function noticeUpd() {
     })
       .then((res) => res.json())
       .then(function (myJson) {
-        console.log(myJson);
-        location.href = `/notice`;
-      });
+        console.log(myJson)
+        location.href = `/notice`
+      })
   }
 }
 //문의사항
 
-const question_write_form = document.querySelector("#question_write_form");
+const question_write_form = document.querySelector("#question_write_form")
 
 function question_write_submit_btn() {
   if (!question_write_form.question_pk.value) {
-    questionReg();
-    return;
+    questionReg()
+    return
   }
-  questionUpd();
+  questionUpd()
 }
 
 function questionReg() {
-  ajax();
+  ajax()
   function ajax() {
     let param = {
       question_title: question_write_form.question_title.value,
       question_ctnt: question_write_form.question_ctnt.value,
       question_userpk: question_write_form.question_userpk.value,
       question_typ: question_write_form.question_typ.value,
-    };
+    }
     fetch("/question_write", {
       method: "post",
       headers: {
@@ -198,24 +198,24 @@ function questionReg() {
     })
       .then((res) => res.json())
       .then(function (myJson) {
-        console.log(myJson);
+        console.log(myJson)
         if (myJson.result === 1) {
-          location.href = `/question`;
+          location.href = `/question`
         }
-      });
+      })
   }
 }
 
 //공지사항 수정
 function questionUpd() {
-  ajax();
+  ajax()
   function ajax() {
     let param = {
       question_title: question_write_form.question_title.value,
       question_ctnt: question_write_form.question_ctnt.value,
       question_pk: question_write_form.question_pk.value,
-    };
-    console.log(param);
+    }
+    console.log(param)
     fetch("/question_upd", {
       method: "post",
       headers: {
@@ -225,8 +225,8 @@ function questionUpd() {
     })
       .then((res) => res.json())
       .then(function (myJson) {
-        console.log(myJson);
-        location.href = `/question`;
-      });
+        console.log(myJson)
+        location.href = `/question`
+      })
   }
 }
--- src/main/resources/static/js/cs/notice.js
+++ src/main/resources/static/js/cs/notice.js
@@ -2,78 +2,78 @@
 
 //notice
 
-let cs_notice_titlebar = document.querySelectorAll(".cs_notice_titlebar");
-let cs_notice_detail_close = document.querySelectorAll(".cs_notice_detail");
+let cs_notice_titlebar = document.querySelectorAll(".cs_notice_titlebar")
+let cs_notice_detail_close = document.querySelectorAll(".cs_notice_detail")
 
 for (let i = 0; i < cs_notice_titlebar.length; i++) {
-  let cs_notice_titlebarEle = cs_notice_titlebar[i];
+  let cs_notice_titlebarEle = cs_notice_titlebar[i]
   cs_notice_titlebarEle.addEventListener("click", function () {
-    let cs_notice_detail = this.nextSibling.nextSibling;
-    let notice_view = this.childNodes.item(3).childNodes.item(3);
+    let cs_notice_detail = this.nextSibling.nextSibling
+    let notice_view = this.childNodes.item(3).childNodes.item(3)
     if (cs_notice_detail.style.height < "100px") {
       for (let j = 0; j < cs_notice_detail_close.length; j++) {
-        cs_notice_titlebar[j].style.backgroundColor = "rgb(255, 255, 255)";
-        cs_notice_titlebar[j].style.color = "rgb(0, 0, 0)";
-        cs_notice_detail_close[j].style.height = "0em";
-        cs_notice_detail_close[j].style.padding = "0px";
+        cs_notice_titlebar[j].style.backgroundColor = "rgb(255, 255, 255)"
+        cs_notice_titlebar[j].style.color = "rgb(0, 0, 0)"
+        cs_notice_detail_close[j].style.height = "0em"
+        cs_notice_detail_close[j].style.padding = "0px"
       }
-      let notice_pk = cs_notice_titlebarEle.dataset.pk;
+      let notice_pk = cs_notice_titlebarEle.dataset.pk
       fetch(`/updNoticeView?notice_pk=${notice_pk}`, {
         method: "put",
       })
         .then(function (res) {
-          return res.json();
+          return res.json()
         })
         .then((myJson) => {
           if (myJson.result == 1) {
-            notice_view.innerHTML = myJson.notice_view;
+            notice_view.innerHTML = myJson.notice_view
           }
-        });
+        })
       for (let i = 0; i <= 25; i++) {
         setTimeout(function () {
-          cs_notice_detail.style.height = `${i}em`;
-        });
+          cs_notice_detail.style.height = `${i}em`
+        })
       }
-      cs_notice_titlebarEle.style.backgroundColor = "rgb(82, 0, 121)";
-      cs_notice_titlebarEle.style.color = "rgb(255, 255, 255)";
-      cs_notice_detail.style.paddingTop = "1em";
-      cs_notice_detail.style.paddingBottom = "1em";
+      cs_notice_titlebarEle.style.backgroundColor = "rgb(82, 0, 121)"
+      cs_notice_titlebarEle.style.color = "rgb(255, 255, 255)"
+      cs_notice_detail.style.paddingTop = "1em"
+      cs_notice_detail.style.paddingBottom = "1em"
     } else if (cs_notice_detail.style.height > "100px") {
       for (let i = 25; i >= 0; i--) {
         setTimeout(function () {
-          cs_notice_detail.style.height = `${i}em`;
-        });
+          cs_notice_detail.style.height = `${i}em`
+        })
       }
-      cs_notice_titlebarEle.style.backgroundColor = "rgb(255, 255, 255)";
-      cs_notice_titlebarEle.style.color = "rgb(0, 0, 0)";
-      cs_notice_detail.style.padding = "0px";
+      cs_notice_titlebarEle.style.backgroundColor = "rgb(255, 255, 255)"
+      cs_notice_titlebarEle.style.color = "rgb(0, 0, 0)"
+      cs_notice_detail.style.padding = "0px"
       //location.href = `notice`;
     }
-  });
+  })
 }
 
 //공지사항 삭제\
 function cs_del_btn(notice_pk) {
   if (alert("삭제 하시겠습니까?")) {
-    return;
+    return
   }
-  console.log(notice_pk);
+  console.log(notice_pk)
   fetch(`/notice_del?notice_pk=${notice_pk}`, {
     method: "delete",
   })
     .then(function (res) {
-      return res.json();
+      return res.json()
     })
     .then(function (myJson) {
-      console.log(myJson);
+      console.log(myJson)
       if (myJson.result === 1) {
         //삭제 완료
-        location.href = `notice`;
+        location.href = `notice`
       } else {
         //삭제 실패
-        alert("삭제 실패하였습니다.");
+        alert("삭제 실패하였습니다.")
       }
-    });
+    })
 }
 
 /*
