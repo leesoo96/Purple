5368b6290051e74b3b17bd6d3439af312f9a491e
leesoojin961202 <dltnwls7042@gmail.com>
2021-03-10 19:48:00 +0900

    2021.03.10

--- src/main/resources/static/js/common/temp.js
+++ src/main/resources/static/js/common/temp.js
@@ -118,11 +118,13 @@ for (let i = 0; i < c_div.length; i++) {
 // feed_content 클릭 이벤트
 let feed_content = document.querySelector(".feed_content")
 let feed_overlay = document.querySelector(".feed_overlay")
-feed_content.addEventListener("click", function () {
+
+feed_content.onclick = function () {
   feed_overlay.style.display = "block"
-})
+}
 
 const feed_overlay_close = document.querySelector(".fa-times")
-feed_overlay_close.addEventListener("click", function () {
+console.log(feed_overlay_close)
+feed_overlay_close.onclick = function () {
   feed_overlay.style.display = "none"
-})
+}
