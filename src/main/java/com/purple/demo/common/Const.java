47dddc1f48ea7dc80c8370384b845d8d4053a84f
ash0419 <ash0419@naver.com>
2021-03-12 21:05:54 +0900

    2021.03.12

--- src/main/java/com/purple/demo/common/Const.java
+++ src/main/java/com/purple/demo/common/Const.java
@@ -3,7 +3,10 @@ package com.purple.demo.common;
 public class Const {
 
 	public static final String KEY_LOGINUSER = "loginUser";
-
 	// 피드
 	public static final String KEY_FEEDLISTDATA = "feedListData";
+
+	// ajax통신할 때 결과값
+	public static final String KEY_REUSLT = "result";
+
 }
