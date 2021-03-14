60bc2690771ad4f2db7400635aea0b163876f012
developer-choihongseok <chs02305@naver.com>
2021-03-14 18:38:17 +0900

    2021.03.14

--- src/main/java/com/purple/demo/controller/FeedController.java
+++ src/main/java/com/purple/demo/controller/FeedController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.purple.demo.common.Const;
 import com.purple.demo.model.FeedDomain;
@@ -25,12 +29,13 @@ public class FeedController {
 		return "/feed";
 	}
 
-	/*
+	// 피드
+	@ResponseBody
 	@GetMapping("")
-	public String selFeedList(Model model, FeedDomain param){
-		List<FeedDomain> feedList = feedservice.selFeedList(param);
-		model.addAttribute(Const.KEY_FEEDLISTDATA, feedList);
+	public Map<String, Object> selFeedList(Model model, @RequestBody FeedDomain param){
+		Map<String, Object> FeedListResult = new HashMap<String, Object>();
+		FeedListResult.put(Const.KEY_FEEDLISTDATA, feedservice.selFeedList(param));
 
-		return "/feed";
-	} */
+		return FeedListResult;
+	}
 }
