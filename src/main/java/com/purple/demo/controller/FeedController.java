fefef205c9d93385fe3bc322d8b4a3f6b2dbcd26
ash0419 <ash0419@naver.com>
2021-03-18 17:31:14 +0900

    2021.03.18

--- src/main/java/com/purple/demo/controller/FeedController.java
+++ src/main/java/com/purple/demo/controller/FeedController.java
@@ -1,17 +1,11 @@
 package com.purple.demo.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.purple.demo.model.FeedEntity;
 import com.purple.demo.service.FeedService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/feed")
@@ -27,10 +21,10 @@ public class FeedController {
 	}
 
 //  피드 글업로드
-	@ResponseBody
-	@PostMapping("/uploadfeed")
-	public Map<String, Object> uploadfeed(@RequestBody FeedEntity p) {
-		Map<String, Object> feedResult = new HashMap<String, Object>();
-		return feedResult;
-	}
+	// @ResponseBody
+	// @PostMapping("/uploadfeed")
+	// public Map<String, Object> uploadfeed(@RequestBody FeedEntity p) {
+	// 	Map<String, Object> feedResult = new HashMap<String, Object>();
+	// 	return feedResult;
+	// }
 }
