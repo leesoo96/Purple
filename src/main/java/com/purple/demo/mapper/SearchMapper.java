c6d3f002af28c66c1ba8fa1f5921623793194dd2
aofhd0543 <aofhd0543@naver.com>
2021-03-24 22:48:28 +0900

    2021.03.24

--- src/main/java/com/purple/demo/mapper/SearchMapper.java
+++ src/main/java/com/purple/demo/mapper/SearchMapper.java
@@ -3,6 +3,7 @@ package com.purple.demo.mapper;
 import java.util.List;
 
 import com.purple.demo.model.FeedListDTO;
+import com.purple.demo.model.HashtagEntity;
 import com.purple.demo.model.DTO.SearchUserDTO;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -12,4 +13,6 @@ public interface SearchMapper {
     List<SearchUserDTO> selUserSearch(SearchUserDTO dto);
 
     List<FeedListDTO> selFeedSearch(FeedListDTO dto);
+
+    List<FeedListDTO> searchHashtag(HashtagEntity dto);
 }
