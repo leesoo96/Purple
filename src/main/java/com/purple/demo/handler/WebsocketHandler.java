e2b172fa06e19c0c160d4241287f4e8a78fbc539
developer-choihongseok <chs02305@naver.com>
2021-03-27 00:26:45 +0900

    2021.03.26

--- src/main/java/com/purple/demo/handler/WebsocketHandler.java
+++ src/main/java/com/purple/demo/handler/WebsocketHandler.java
@@ -10,12 +10,12 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
-public class WebsocketHandler extends TextWebSocketHandler {
+public class WebsocketHandler extends TextWebSocketHandler {    // 웹 소켓 서버 생성하는 곳.
     
     // 접속한 클라이언트 세션들을 저장하는 list 
     private static List<WebSocketSession> list = new ArrayList<>();
 
-    // 클라이언트가 접속했을때 호출된다
+    // 클라이언트가 접속했을 때 호출된다
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         list.add(session);
@@ -23,7 +23,7 @@ public class WebsocketHandler extends TextWebSocketHandler {
         System.out.println("연결성공");
     }
 
-    // 클라이언트가 메시지를 보냈을때 호출된다 
+    // 클라이언트가 메시지를 보냈을 때 호출된다 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // 전송된 메시지를 list의 모든 세션에 전송
