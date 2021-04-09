a37f0ca37bea588451445be8039aed733edf1329
leesoojin961202 <dltnwls7042@gmail.com>
2021-04-09 19:50:13 +0900

    2021.04.09_final

--- src/main/java/com/purple/demo/config/WebConfiguration.java
+++ src/main/java/com/purple/demo/config/WebConfiguration.java
@@ -1,8 +1,5 @@
 package com.purple.demo.config;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,14 +9,14 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-	@Value("${spring.servlet.multipart.location}")
-	private String uploadImgPath;
+	@Value("${spring.servlet.multipart.location}") 
+   private String uploadImagePath;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {	
 			
 		registry.addResourceHandler("/images/**")
-				.addResourceLocations("file:///" + uploadImgPath + "/images/")
+				.addResourceLocations("file:///" + uploadImagePath + "/images/")
 				.setCachePeriod(3600)
 				.resourceChain(true)
 				.addResolver(new PathResourceResolver());
